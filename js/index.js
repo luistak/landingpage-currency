4c7c2ad2f7a7f9350181fe907668dded5e8d8227
Luis Takahashi <takahashihideki408@gmail.com>
2018-08-04 14:50:38 -0300

    Adições de libs e setup inicial

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 gama = [];
 
+gama.initialize = function() {
+    gama.setup();
+}
 
-$(function () {
-    
-})
+gama.setup = function () {
+    let oReq = new XMLHttpRequest();
+
+    let url = 'https://api.coinmarketcap.com/v1/ticker/?convert=BRL&limit=10';
+
+    oReq.open("GET", url, true);
+    oReq.responseType = "json";
+    oReq.onload = function(e) {
+    var json = oReq.response; // não é responseText
+    console.log(e);
+    console.log(json);
+    }
+    oReq.send();
+}
+
+gama.initialize();
